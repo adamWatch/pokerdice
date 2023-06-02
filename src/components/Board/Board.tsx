3bf3677bdac617c92f84e1bc25185b19cae2bc84
Adam Watchmaker <watchmaker1337@gmail.com>
2023-06-02 17:51:28 +0200

    add Board look

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,39 +1,110 @@
-import React from 'react';
+/* eslint-disable react/no-array-index-key */
+import React, { useState } from 'react';
 import './board.css';
 import { Dice } from '../Dice/Dice';
 import { TickField } from '../TickField/TickField';
 
 export function Board() {
+  const [isBetting, setIsBetting] = useState(true);
+  const [listOfDicesPl, setListOfDicesPl] = useState<number[]>([]);
+  const [resultPl, setResultPl] = useState('nothing');
+  const [listOfDicesAi, setListOfDicesAi] = useState<number[]>([]);
+  const [resultAi, setResultAi] = useState('nothing');
+  const [tickFieldValues, setTickFieldValues] = useState<boolean[]>([false, false, false, false, false, false]);
+  const handleTickFieldChange = (index:number) => {
+    const newTickFieldValues = [...tickFieldValues];
+    newTickFieldValues[index] = !newTickFieldValues[index];
+    setTickFieldValues(newTickFieldValues);
+  };
+  const [money, setMoney] = useState(100);
+  const [bet, setBet] = useState(1);
+  const handleBetChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    let stake = Number(event.target.value);
+    if (stake > money) {
+      stake = money;
+    } else if (stake < 1) {
+      stake = 1;
+    }
+    setBet(stake);
+  };
+  const closeBetting = () => {
+    setIsBetting(false);
+  };
+  const throwDices = (amountOfDices:6) => {
+    const listOfDices = [];
+    for (let i = 0; i < amountOfDices; i++) {
+      const dice = Math.floor(Math.random() * 6) + 1;
+      listOfDices.push(dice);
+    }
+    console.log(listOfDices);
+    return listOfDices;
+  };
+  const throwAfterSetStake = () => {
+    const listOfDicesPl = throwDices(6);
+    const listOfDicesAi = throwDices(6);
+    setListOfDicesPl(listOfDicesPl);
+    setListOfDicesAi(listOfDicesAi);
+    setMoney(money - bet);
+    closeBetting();
+  };
+  console.log(listOfDicesPl);
+  if (isBetting) {
+    return (
+      <div className="board betting">
+        <div className="betting__container">
+          <p>
+            Your Money:
+            <span className="money">{money}</span>
+          </p>
+          <p>
+            Stake:
+            <input type="number" value={bet} onChange={handleBetChange} />
+          </p>
+        </div>
+        <div className="btn__container">
+          <button type="button" className="btn btn_in_betting" onClick={throwAfterSetStake}>Throw Dice</button>
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="board">
-      <h2>Ai Dices</h2>
+      <h2 className="player">Ai Dices</h2>
       <div className="dices__row ai">
-        <Dice diceNo="four" />
-        <Dice diceNo="four" />
-        <Dice diceNo="four" />
-        <Dice diceNo="four" />
-        <Dice diceNo="four" />
-        <Dice diceNo="four" />
+        {listOfDicesAi.map((dice, index) => <Dice diceNo={dice} key={index + 6} />)}
 
       </div>
+      <div className="result__container">
+        Result:
+        {' '}
+        <span className="result">{resultAi}</span>
+      </div>
 
-      <h2>Player Dices</h2>
+      <div className="btn_result_container">
+        <div className="handle__game win">
+          <p>Winner is:</p>
+          <span>You</span>
+        </div>
+        <div className="button__container">
+          <button className="btn" type="button">Throw dice</button>
+          <button className="btn" type="button">Continue</button>
+        </div>
+      </div>
+
+      <h2 className="player">Player Dices</h2>
       <div className="dices__row player">
-        <Dice diceNo="four" />
-        <Dice diceNo="four" />
-        <Dice diceNo="four" />
-        <Dice diceNo="four" />
-        <Dice diceNo="four" />
-        <Dice diceNo="four" />
+        {listOfDicesPl.map((dice, index) => <Dice diceNo={dice} key={index} />)}
 
       </div>
+
       <div className="tick-field__row">
-        <TickField />
-        <TickField />
-        <TickField />
-        <TickField />
-        <TickField />
-        <TickField />
+        {tickFieldValues.map((tickField, index) => <TickField index={index} setChecked={handleTickFieldChange} key={index + 12} />)}
+
+      </div>
+      <div className="result__container">
+        Result:
+        {' '}
+        <span className="result">{resultPl}</span>
       </div>
     </div>
   );
