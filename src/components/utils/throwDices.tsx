588557587ee1180b97a2f19c882a6ebe72254c78
Adam Watchmaker <watchmaker1337@gmail.com>
2023-07-20 16:55:52 +0200

    reclude from board

---
+++ src/components/utils/throwDices.tsx
@@ -0,0 +1,9 @@
+export const throwDices = (amountOfDices:number) => {
+  const listOfDices = [];
+  for (let i = 0; i < amountOfDices; i++) {
+    const dice = Math.floor(Math.random() * 5) + 1;
+    listOfDices.push(dice);
+  }
+  console.log(listOfDices);
+  return listOfDices;
+};
